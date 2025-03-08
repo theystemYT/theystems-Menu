4ca1bfcd39acdf82a9abcaf01d2920beef29ee97
theystemYT <cabroncitohernandez06@gmail.com>
2025-03-08 08:47:56 -0800

    Update theystem's Menu.lua

--- theystem's Menu.lua
+++ theystem's Menu.lua
@@ -204,29 +204,142 @@ Tabs.Scripts:CreateButton({
     end
 })
 
-Tabs.Cameras:CreateToggle({
-    Name = "Freeze Camera (a little bit broken)",
-    CurrentValue = toggles.FreezeCamera,
-    Callback = function(value)
-        toggles.FreezeCamera = value
-        local camera = game.Workspace.CurrentCamera
-        local Player = game.Players.LocalPlayer
-        if value then
-            local frozenCFrame = camera.CFrame
-            game:GetService("RunService").RenderStepped:Connect(function()
-                if toggles.FreezeCamera then
-                    camera.CFrame = frozenCFrame
-                end
-            end)
-            frozenCFrame = camera.CFrame
+Tabs.Scripts:CreateButton({
+    Name = "Gunfight Arena Script",
+    Callback = function()
+        local game = game
+        local players = game:GetService("Players")
+        local player = players.LocalPlayer
+        local UIS = game:GetService("UserInputService")
+        local rs = game:GetService("RunService")
+        local cg = cloneref(game:GetService("CoreGui"))
+        local camera = workspace.CurrentCamera
+        local instance = Instance.new
+        local udim2 = UDim2.new
+        local vector2 = Vector2.new
+        local vector3 = Vector3.new
+        local color3 = Color3.new
+        local cframe = CFrame.new
+        local enum = Enum.KeyCode
+        local silentaim = false
+        local silentkeybindtoggle = false
+        local silentkeybind = false
+        local noforcefields = false
+        local weapons = {}
+        for _,v in pairs(game:GetService("ReplicatedStorage").Weapons:GetChildren()) do
+            table.insert(weapons,v.Name)
         end
-        
-        Rayfield:Notify({
-            Title = "Freeze Camera " .. (value and "Enabled" or "Disabled"),
-            Content = "Your camera is now " .. (value and "frozen." or "unfrozen."),
-            Duration = 3,
-            Image = value and "check" or "x"
-        })
+        local camos = {}
+        for _,v in pairs(game:GetService("ReplicatedStorage").Camos:GetChildren()) do
+            table.insert(camos,v.Name)
+        end
+        local primary
+        local secondary
+        local primarycamo
+        local secondarycamo
+
+        local s=player.PlayerScripts.Vortex.Modifiers.Steadiness
+        local m=player.PlayerScripts.Vortex.Modifiers.Mobility
+        local function r()
+        if s and s.Value>0 then s.Value=0 end
+        if m and m.Value>0 then m.Value=0 end
+        end
+        if s then s.Changed:Connect(r) end
+        if m then m.Changed:Connect(r) end
+        r()
+
+        local function visible(origin, direction, target, ignore)
+            local params = RaycastParams.new()
+
+            local filterList = {game.Players.LocalPlayer.Character, target}
+            if ignore then
+                for _, v in ipairs(ignore) do
+                    table.insert(filterList, v)
+                end
+            end
+
+            params.FilterDescendantsInstances = filterList
+            return (not workspace:Raycast(origin, direction, params))
+        end
+
+        local closestPlayer = nil
+        local team = nil
+
+        local function getClosestPlayer()
+            team = player:GetAttribute("Team")
+            local closest, distance = nil, math.huge
+
+            for _, character in ipairs(workspace:GetChildren()) do
+                local humanoid = character:FindFirstChild("Humanoid")
+                if character and humanoid and humanoid.Health > 0 then
+                    local player = players:FindFirstChild(character.Name)
+                    if player and player:GetAttribute("Team") ~= team then
+                        local head = character:FindFirstChild("Head")
+                        if head then
+                            local w2s, onscreen = camera:WorldToViewportPoint(head.Position)
+                            if onscreen then
+                                local dist = (vector2(w2s.X, w2s.Y) - vector2(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)).Magnitude
+                                if dist < distance then
+                                    local origin = camera.CFrame.Position
+                                    local direction = head.Position - origin
+                                    if visible(origin, direction, character, nil) then
+                                        closest = character
+                                        distance = dist
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            return closest
+        end
+
+        rs.RenderStepped:Connect(function()
+            closestPlayer = getClosestPlayer()
+        end)
+
+        local oldIndex
+        oldIndex = hookmetamethod(game, "__index", function(self, index)
+            local func = debug.getinfo(3, "n")
+            if func and func.name then
+                if func.name == "Fire" and index == "CFrame" and closestPlayer then
+                    local head = closestPlayer:FindFirstChild("Head")
+                    if head then
+                        local origin = camera.CFrame.Position
+                        local direction = head.Position - origin
+                        if visible(origin, direction, closestPlayer, nil) and silentaim then
+                            if silentkeybindtoggle then
+                                if silentkeybind then
+                                    return cframe(head.Position)
+                                end
+                            else
+                                return cframe(head.Position)
+                            end
+                        end
+                    end
+                end
+            end
+            return oldIndex(self, index)
+        end)
+
+        rs.RenderStepped:Connect(function()
+            for _,v in pairs(game:GetService("Workspace").Env:GetChildren()) do
+                if string.find(v.Name, "Forcefield") and noforcefields then
+                    if v.FullSphere.Color ~= Color3.fromRGB(0, 102, 255) then
+                        v:Destroy()
+                    end
+                end
+            end
+        end)
+    end
+})
+
+Tabs.Scripts:CreateButton({
+    Name = "Airsoft FE Script",
+    Callback = function()
+        Rayfield:Destroy()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Airsoft-FE-Script-Hub-25137"))()
     end
 })
 
