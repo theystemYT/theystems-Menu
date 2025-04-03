08634a99b4e92f08d30eede1260ede9aad3950bf
theystemYT <cabroncitohernandez06@gmail.com>
2025-04-02 19:22:36 -0700

    Update theystem's Menu.lua

--- theystem's Menu.lua
+++ theystem's Menu.lua
@@ -226,7 +226,7 @@ Tabs.MenuSettings:CreateButton({
 })
 
 Tabs.Credits:CreateSection("Credits")
-Tabs.Credits:CreateLabel("Credits to @theystem for making this script. Click on the button below to copy @theystem's YouTube channel link.")
+Tabs.Credits:CreateLabel("Credits to @theystem for making this script. Click on the button below to copy the links for my GitHub account, ScriptBlox account, and YouTube account.")
 Tabs.Credits:CreateButton({
     Name = "Copy YouTube Channel Link",
     Callback = function()
