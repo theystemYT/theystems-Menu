8a5b9574d5f287c8771c4f0a2a9d32754ffbf99e
theystemYT <cabroncitohernandez06@gmail.com>
2025-04-26 21:13:13 -0700

    Update theystem's Menu.lua

--- theystem's Menu.lua
+++ theystem's Menu.lua
@@ -40,7 +40,7 @@ Tabs.Scripts:CreateButton({
 Tabs.Scripts:CreateButton({
     Name = "Dex",
     Callback = function()
-        loadstring(game:HttpGet("https://github.com/LorekeeperZinnia/Dex/blob/master/main.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LorekeeperZinnia/Dex/refs/heads/master/main.lua"))()
     end
 })
 
