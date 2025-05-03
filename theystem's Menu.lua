3ccfa6fd00440339ae5dcc5f6fa1e215b69710d7
theystemYT <cabroncitohernandez06@gmail.com>
2025-05-03 04:09:57 -0700

    Update theystem's Menu.lua

--- theystem's Menu.lua
+++ theystem's Menu.lua
@@ -8,7 +8,7 @@ local MarketplaceService = game:GetService("MarketplaceService")
 
 local Window = Rayfield:CreateWindow({
     Name = "theystem's Menu",
-    Icon = 0,
+    Icon = 94753139016845,
     LoadingTitle = "Loading theystem's Menu...",
     LoadingSubtitle = "Credits are in Credits Tab",
     Theme = "AmberGlow",
