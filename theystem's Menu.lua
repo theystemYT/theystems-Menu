5eca88323c8678d9120be9b2964da0f800b63a7d
theystemYT <cabroncitohernandez06@gmail.com>
2025-05-18 01:40:19 -0700

    Update theystem's Menu.lua

--- theystem's Menu.lua
+++ theystem's Menu.lua
@@ -25,21 +25,6 @@ local Window = Rayfield:CreateWindow({
     }
 })
 
-if not getgenv().theystemPromptShown then
-    getgenv().theystemPromptShown = true
-    local prompt = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareLtd/Sirius/refs/heads/request/prompt.lua"))()
-    prompt.create(
-        "theystem's Menu",
-        [[Welcome to theystem's Menu! Thank you for using this script.
-
-<font transparency='0.4'>To exit this screen, press Continue.</font>]],
-        "Continue",
-        "",
-        function(result)
-        end
-    )
-end
-
 local Tabs = {
     Scripts = Window:CreateTab("Universal Scripts", "terminal"),
     UISettings = Window:CreateTab("UI Settings", "settings"),
