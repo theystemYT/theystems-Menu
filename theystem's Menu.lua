206d102ea3deab0d52ba17d6256c64c12d73aa54
theystemYT <cabroncitohernandez06@gmail.com>
2025-05-19 00:36:50 -0700

    Update theystem's Menu.lua

--- theystem's Menu.lua
+++ theystem's Menu.lua
@@ -41,31 +41,39 @@ Tabs.Scripts:CreateButton({
 Tabs.Scripts:CreateButton({
     Name = "Force Reset Character",
     Callback = function()
-        if plr.Character and plr.Character:FindFirstChild("Humanoid") then
-            local prompt = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareLtd/Sirius/refs/heads/request/prompt.lua"))()
-            prompt.create(
-                '',
-                [[Are you sure you want to reset your character?
+        local prompt = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareLtd/Sirius/refs/heads/request/prompt.lua"))()
+        prompt.create(
+            '',
+            [[Are you sure you want to reset your character?
 
 <font transparency='0.4'>Note: This can cause stuff like not being able to respawn (in certain games). You also may experience not being able to respawn using Respawn in the Roblox menu if this does happen (in certain games).</font>]],
-                'Reset',
-                'Cancel',
-                function(result)
-                    if result == "Reset" then
+            'Reset',
+            'Cancel',
+            function(result)
+                if result == "Reset" then
+                    if not plr.Character then
+                        Rayfield:Notify({
+                            Title = "Error",
+                            Content = "No character was found.",
+                            Duration = 3,
+                            Image = "circle-alert",
+                        })
+                    elseif not plr.Character:FindFirstChild("Humanoid") then
+                        Rayfield:Notify({
+                            Title = "Error",
+                            Content = "No humanoid was found.",
+                            Duration = 3,
+                            Image = "circle-alert",
+                        })
+                    else
                         plr.Character.Humanoid:TakeDamage(plr.Character.Humanoid.Health)
                     end
                 end
-            )
-        else
-            Rayfield:Notify({
-                Title = "Error",
-                Content = "No humanoid was found.",
-                Duration = 3,
-                Image = "circle-alert",
-            })
-        end
+            end
+        )
     end,
 })
+
 
 Tabs.Scripts:CreateButton({
     Name = "Hydra VIP Script (FORTLINE)",
