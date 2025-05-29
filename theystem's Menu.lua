1033b23725863ba3a65af3d489dbd2e04933278c
theystemYT <cabroncitohernandez06@gmail.com>
2025-05-29 00:01:09 -0700

    Update theystem's Menu.lua

--- theystem's Menu.lua
+++ theystem's Menu.lua
@@ -59,7 +59,7 @@ Tabs.Scripts:CreateButton({
         local prompt = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareLtd/Sirius/refs/heads/request/prompt.lua"))()
         prompt.create(
 		'Confirmation',
-[[Are you sure you want to reset your character?
+        [[Are you sure you want to reset your character?
 
 <font transparency='0.4'>Note: This may cause respawn issues in certain games, or not work at all. This prompt is also added for accidental clicks.</font>]],
             'Reset',
