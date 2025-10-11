5f718a99c3bd0b8c1e8991f353906a99dc8f23f1
theystemYT <cabroncitohernandez06@gmail.com>
2025-10-10 20:43:49 -0700

    Delete ​.lua

--- ​.lua
+++
@@ -1,4 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local NotifsEvent2 = ReplicatedStorage.Remotes.NotifsEvent2
-
-firesignal(NotifsEvent2.OnClientEvent, "[theystem]: Stop playing this fucking brainrot game plz", "Rainbow")
