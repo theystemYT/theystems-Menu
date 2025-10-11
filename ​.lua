ffa9aabfc4dcdc8ad1f12dcb1cc3eaddcb296334
theystemYT <cabroncitohernandez06@gmail.com>
2025-10-10 20:43:36 -0700

    Add files via upload

---
+++ ​.lua
@@ -0,0 +1,4 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local NotifsEvent2 = ReplicatedStorage.Remotes.NotifsEvent2
+
+firesignal(NotifsEvent2.OnClientEvent, "[theystem]: Stop playing this fucking brainrot game plz", "Rainbow")
